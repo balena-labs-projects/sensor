b8b589f6dd82746f0ab2234a4f767f62f5451477
Alan Boris <39385685+alanb128@users.noreply.github.com>
2021-05-26 14:00:15 -0400

    Add short UUID, sensor ID

--- reading.py
+++ reading.py
@@ -24,6 +24,9 @@ class Reading:
     def write_reading(self):
         """Write the readings for the current context."""
 
+        sensor_id = os.getenv('SENSOR_ID', '')
+        UUID = os.environ.get('RESIN_DEVICE_UUID')[:7] # First seven chars of device UUID
+        
         if os.getenv('COLLAPSE_FIELDS', 0) == "1":  # return just a dict of fields
             reading = {}
             for dev in self.context.devices:
@@ -33,6 +36,14 @@ class Reading:
                 else:
                     # send the device name and fields to the transform function
                     new_fields = device_transform(dev.name, device_fields)
+                    
+                # If sensor ID defined, add to fields:
+                if sensor_id != '':
+                    reading.update({"sensor_id": sensor_id})
+                    
+                # Add short UUID as field
+                reading.update({"short_uuid": UUID})   
+                          
                 reading.update(new_fields)  # merges dicts, but overwrites equal values    
 
         else:   # return measurements and fields in a list
@@ -46,6 +57,15 @@ class Reading:
                     new_fields = device_transform(dev.name, device_fields)
                 reading2 = {"measurement": dev.name, "fields": new_fields}
                 reading.append(reading2)
+                
+                # If sensor ID defined, add to fields:
+                if sensor_id != '':
+                    reading3 = {"measurement": "sensor_ID", "fields": {"sensor_id": sensor_id}}
+                    reading.append(reading3)
+                    
+                # Add short UUID to fields:
+                reading3 = {"measurement": "short_UUID", "fields": {"short_uuid": UUID}}
+                reading.append(reading3)
 
         return reading
 
